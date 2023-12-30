8b80d0707e2bb29da520ce244621484b2530e692
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-12-30 15:28:37 +0000

    deploy Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/25/hello-world/"];function toRandomPost(){
+var posts=["2023/12/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
